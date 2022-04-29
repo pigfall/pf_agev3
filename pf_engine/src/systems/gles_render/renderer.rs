175fdd9d45946c12fe81d2d79bf003daf9b46b75
tzz <koi_town@outlook.com>
2022-04-30 00:15:17 +0800

    gles_renderer: impl Sync for renderer

--- pf_engine/src/systems/gles_render/renderer.rs
+++ pf_engine/src/systems/gles_render/renderer.rs
@@ -1,10 +1,17 @@
 
 pub struct Renderer{
-
+    //egl: Egl14,
 }
 
 impl Renderer{
     pub fn new()->Self{
-        Self{}
+        Self{
+            //egl: Egl14::entry_load().unwrap(),
+
+        }
     }
 }
+
+
+unsafe impl Send for Renderer{}
+unsafe impl Sync for Renderer{}
